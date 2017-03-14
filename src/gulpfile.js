d234dbfa2cc3770a319b67cefe4a2f7e19eaf4eb
Alberto <alber7rp@gmail.com>
2017-03-14 23:08:12 +0000

    añadidas tareas gulp

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -26,8 +26,26 @@ gulp.task('routing',shell.task([
   'nodejs routing/routing.js'
 ])
 );
- 
+
 gulp.task('router',shell.task([
   'nodejs router/route.js'
+])
+);
+
+
+//sessions
+
+gulp.task('management',shell.task([
+  'node cookie_management/cookie_management.js'
+])
+);
+
+gulp.task('cookie_module',shell.task([
+  'node cookie_module/cookie_module.js'
+])
+);
+
+gulp.task('auth',shell.task([
+  'node session_auth/session_auth.js'
 ])
 );
